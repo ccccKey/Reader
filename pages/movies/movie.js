7fd7fd20085191205f1e071ff4624bb103cad5c7
ccccKey <670897678@qq.com>
2017-06-14 19:40:48 +0800

    2017-6-14
    test request douban API

--- pages/movies/movie.js
+++ pages/movies/movie.js
@@ -1,4 +1,24 @@
 Page({
 
-  
+  onLoad:function(event){
+    wx.request({
+      url: 'https://api.douban.com/v2/book/1220562',
+
+      success:function(event)
+      {
+        console.log("OnSuccess");
+        console.log(event.data);
+      },
+      fail:function(event)
+      {
+        console.log("OnFail");
+      },
+      complete:function(event)
+      {
+        console.log("OnComplete");
+      }
+
+    })
+  }
+
 })
