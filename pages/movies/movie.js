4923b4cd9db2da919876fd89877d0d521f215d34
ccccKey <670897678@qq.com>
2017-06-15 19:25:32 +0800

    2017-6-15
    get all movie data from douban.com which I need

--- pages/movies/movie.js
+++ pages/movies/movie.js
@@ -2,17 +2,24 @@ var app = getApp();
 
 Page({
 
-  onLoad:function(event){
-    var hotListUrl = "/movie/in_theaters";
-    var willPublichLietUrl = "/movie/coming_soon";
-    var top250ListUrl = "/movie/top250";
-
-    this.getCurrListData(app.globalData.g_doubanHomePage + hotListUrl);
-    this.getCurrListData(app.globalData.g_doubanHomePage + willPublichLietUrl);
-    this.getCurrListData(app.globalData.g_doubanHomePage + top250ListUrl);
+  data: {
+    newMovies: {},
+    comingMovies: {},
+    topMovies: {}
   },
 
-  getCurrListData:function(url){
+  onLoad: function (event) {
+    var hotListUrl = "/movie/in_theaters" + "?start=0&count=3";
+    var willPublichLietUrl = "/movie/coming_soon" + "?start=0&count=3";
+    var top250ListUrl = "/movie/top250" + "?start=0&count=3";
+
+    this.getCurrListData(app.globalData.g_doubanHomePage + hotListUrl, "newMovies");
+    this.getCurrListData(app.globalData.g_doubanHomePage + willPublichLietUrl, "comingMovies");
+    this.getCurrListData(app.globalData.g_doubanHomePage + top250ListUrl, "topMovies");
+  },
+
+  getCurrListData: function (url, typeMov) {
+    var that = this;
     wx.request({
       url: url,
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
@@ -20,9 +27,9 @@ Page({
         "Content-Type": "json"
       },
 
-      success: function (event) {
-        console.log("OnSuccess");
-        console.log(event.data);
+      success: function (data) {
+        console.log(data);
+        that.handleData(data.data, typeMov);
       },
       fail: function (event) {
         console.log("OnFail");
@@ -32,6 +39,37 @@ Page({
       }
 
     })
+  },
+
+  handleData: function (currData, typeMov) {
+    var dataList = [];
+
+    for (var idx in currData.subjects) {
+      var obj = currData.subjects[idx];
+      var title = obj.title;
+
+      if (title.length > 6) {
+        title = title.substring(0, 6) + "...";
+      }
+
+      var movie = {
+        title: obj.title,
+        img: obj.images.large,
+        movieId: obj.id,
+        averange: {
+          average: obj.rating.average,
+          stars: obj.rating.stars,
+        }
+      }
+
+      dataList.push(movie);
+    }
+
+    var newMovieList = {};
+    newMovieList[typeMov] = {
+      movies: dataList, 
+    }
+    this.setData(newMovieList);
   }
 
 })
